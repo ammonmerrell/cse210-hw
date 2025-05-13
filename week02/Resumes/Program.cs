59e5f0f667d3a91abfc18335576f35b9346dacd1
ammonmerrell <ammonmerrell@outlook.com>
2025-05-12 19:53:58 -0600

    updated according to solution.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        // job1.Display();        
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        // job2.Display();
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        // This does not work (the command below this line) \/ it was the attempt at storing the job title.
+        // myResume._job[0]._jobTitle = "Microsoft"
+        myResume.Display();
+
+
     }
 }
