cce4e9d365733d172500025e723637be56939016
ammonmerrell <ammonmerrell@outlook.com>
2025-05-12 18:52:47 -0600

    added decorative "-" line to display.

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -12,6 +12,6 @@ public class Job
 
         public void Display()
         {
-            Console.WriteLine($"{_company} ({_jobTitle}) {_startYear} {_endYear}");
+            Console.WriteLine($"{_company} ({_jobTitle}) {_startYear}-{_endYear}");
         }
     }
