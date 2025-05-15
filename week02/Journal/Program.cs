95afb508e636342c30366f5251ec7ec8b4531e22
ammonmerrell <ammonmerrell@outlook.com>
2025-05-15 15:18:59 -0600

    I referenced the properties of the Entry class.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -6,9 +6,11 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
         Journal newJournal = new Journal();
-        newJournal._entry = "hello, today I...";
-        newJournal._timestamp = 2025;
-        newJournal.AddEntry();
+        Entry entry1 = new Entry();
+        entry1._date = "5/15/2025";
+        entry1._promptText = "Who was the most entertaining person today?";
+        entry1._entryText = "Hello, today I ...";
+        entry1.Display();
 
     }
 }
