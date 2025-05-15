33e2a5f161eafc6d01b9a0b1cb0cc49b22ce74a3
ammonmerrell <ammonmerrell@outlook.com>
2025-05-15 15:28:34 -0600

    added PromptGenerater class/object

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,12 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class PromptGenerator
+{
+
+    public List<string> _prompts;
+
+    public string GetRandomPrompt()
+    {
+        return _prompts;
+    }
+}
