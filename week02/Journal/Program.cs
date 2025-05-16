4ca05d13e80c65a770f79ac511e98f3f2a705d7e
ammonmerrell <ammonmerrell@outlook.com>
2025-05-16 09:31:00 -0600

    properly references journal class funtions.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 class Program
 {
@@ -7,12 +8,20 @@ class Program
         Console.WriteLine("Hello World! This is the Journal Project.");
         Journal newJournal = new Journal();
         Entry entry1 = new Entry();
-        PromptGenerator promptGen1 = new PromptGenerator();
+        // PromptGenerator promptGen1 = new PromptGenerator();
         List<string> theList = new List<string>() { "a", "b", "c", "d", "e" };
         string a = "";
-        promptGen1._prompts = theList;
-        a = promptGen1.GetRandomPrompt();
-        Console.WriteLine(a);
+        newJournal._entry = theList;
+        // a = promptGen1.GetRandomPrompt();
+        // Console.WriteLine(a);
+        newJournal.AddEntry(entry1);
+        newJournal.DisplayAll();
 
+        Console.Write("Enter a file to save:");
+        a = Console.ReadLine();
+        newJournal.SaveToFile(a);
+        Console.WriteLine("loading from file...");
+        newJournal.LoadFromFile(a);
+        
     }
 }
