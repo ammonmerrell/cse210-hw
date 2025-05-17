f794b40926b4f1e418c9df52ff28f7a26b03db45
ammonmerrell <ammonmerrell@outlook.com>
2025-05-17 09:20:50 -0600

    Added localvariables in PromptGenerator class and updated the method.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -3,11 +3,12 @@
 public class PromptGenerator
 {
 
-    public List<string> _prompts;
-
-    public string GetRandomPrompt()
+    public List<string> _prompts = new List<string>() { "What was the most exiting thing that happened today?", "What was your favorite activity you particitpated in today?", "Have I seen the Lord's hand in my life today?", "What has been the worst part of the day?", "What was the best food today?" };
+    public Random _randomNumberList = new Random();
+    public int _randomNumber;
+public string GetRandomPrompt()
     {
-        
-        return _prompts[1];
+        _randomNumber = _randomNumberList.Next(1, 6);
+        return _prompts[_randomNumber];
     }
 }
