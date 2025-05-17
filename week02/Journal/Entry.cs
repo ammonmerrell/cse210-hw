a92f70f893048ada2ee2ffc7be356cc1a6bae0e4
ammonmerrell <ammonmerrell@outlook.com>
2025-05-17 09:36:00 -0600

    Added entry localvariables, and time method.

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,7 +1,11 @@
+using System.Security.Cryptography.X509Certificates;
+
 public class Entry
 {
 
     public string _date;
+
+    public DateTime _time = DateTime.Now;
 
     public string _promptText;
     public string _entryText;
@@ -9,5 +13,7 @@ public class Entry
     public void Display()
     {
         Console.WriteLine($"'{_promptText} {_entryText}' was posted on {_date}");
+        _date = _time.ToShortDateString();
+        Console.WriteLine(_date);
     }
 }
