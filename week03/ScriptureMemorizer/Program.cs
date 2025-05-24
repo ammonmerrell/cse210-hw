8b9aa3393a5f32e8ae3287bd88e7678ef55863f9
ammonmerrell <ammonmerrell.outlook.com>
2025-05-24 10:09:45 -0600

    uploaded broken content to github

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        //Scripture aScripture = new Scripture();
+        //string Scrip = "";
+        //int state = 1;
+        //Scrip = aScripture.GetScripture();
+        //Console.WriteLine(Scrip);
+
+        // Reference aRef = new Reference();
+
+        //state = aScripture.HideWord();
+        //if (state == 1)
+        // {
+
+        // }
     }
 }
