5f2dab5a5e3fb1ea9d93bc6b7128dd17920fa6cc
ammonmerrell <ammonmerrell.outlook.com>
2025-05-24 11:26:03 -0600

    Removed commented out potions of program.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,13 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
-        //Scripture aScripture = new Scripture();
-        //string Scrip = "";
-        //int state = 1;
-        //Scrip = aScripture.GetScripture();
-        //Console.WriteLine(Scrip);
+        Scripture aScripture = new Scripture();
+        string Scrip = "";
+        int state = 1;
+        Scrip = aScripture.GetScripture();
+        Console.WriteLine(Scrip);
 
-        // Reference aRef = new Reference();
+        Reference aRef = new Reference();
 
         //state = aScripture.HideWord();
         //if (state == 1)
