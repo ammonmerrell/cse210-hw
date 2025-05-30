f19d306c6a17a338f9260c0a843a9b4ad66ec9d5
ammonmerrell <ammonmerrell.outlook.com>
2025-05-29 19:48:47 -0600

    added geters in produt class

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -17,16 +17,33 @@ class Product
     {
         _name = _n;
     }
+    public string GetName()
+    {
+        return _name;
+    }
+
     public void SetId(string _i)
     {
         _id = _i;
+    }
+    public string GetId()
+    {
+        return _id;
     }
     public void SetQuan(int _q)
     {
         _quantity = _q;
     }
+    public int GetQuan()
+    {
+        return _quantity;
+    }
     public void SetPrice(int _p)
     {
         _price = _p;
+    }
+    public int GetPrice()
+    {
+        return _price;
     }
 }
