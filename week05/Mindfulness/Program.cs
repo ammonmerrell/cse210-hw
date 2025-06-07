d36b7e692f8e0195cb26f914341a99657318d9d7
ammonmerrell <ammonmerrell.outlook.com>
2025-06-06 23:11:35 -0600

    finished program

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,17 +1,43 @@
 using System;
+using System.Data;
 using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
+        string _input = "";
+
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        
         Activity act = new Activity();
         Breathing bre = new Breathing();
         Reflect refl = new Reflect();
         Listing lis = new Listing();
-        // bre.Run();
+        while (_input != "4")
+        {
+        Console.WriteLine("Options:");
+        Console.Write("1. Breathing Activity\n2. Reflecion Activity\n3. Listing Activity\n4. Quit\nType the number: ");
+            _input = Console.ReadLine();
+            if (_input == "1")
+            {
+                bre.Run();
+            }
+            if (_input == "2")
+            {
+                refl.Run();
+            }
+            if (_input == "3")
+            {
+                lis.run(); 
+            }
+        
+          
+        }
+        
     }
 }
