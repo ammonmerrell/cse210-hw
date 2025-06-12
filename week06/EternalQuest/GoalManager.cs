a3801f2876d996cdc949121e763b81faa1da0dd6
ammonmerrell <ammonmerrell.outlook.com>
2025-06-12 10:52:17 -0600

    added goalmanager funtions according to final design flowchart

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -2,5 +2,36 @@ public class GoalManager
 {
     private List<Goal> _goals;
     private int _points;
-    
+    public void run()
+    {
+
+    }
+    public void DisplayPlayerInfo()
+    {
+
+    }
+    public void ListGoalNames()
+    {
+
+    }
+    public void ListGoalDetails()
+    {
+
+    }
+    public void CreateGoal()
+    {
+
+    }
+    public void RecordEvent()
+    {
+
+    }
+    public void SaveGoal()
+    {
+
+    }
+    public void LoadGoal()
+    {
+        
+    }
 }
