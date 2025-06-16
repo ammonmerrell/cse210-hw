df7331a9f592e4a34373c87eb3cd18eb892b879e
ammonmerrell <ammonmerrell.outlook.com>
2025-06-16 17:34:18 -0600

    finneshed swimming

--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -1,4 +1,23 @@
-public class Swimming(string date, int time) : Activity(date, time)
+public class Swimming(string date, int time, double laps) : Activity(date, time)
 {
-    
+    public void Run()
+    {
+        Console.WriteLine(GetSummary());
+    }
+    public override double FindDistance(double laps)
+    {
+        return laps * 50 /1000 * 0.62;
+    }
+    public override double FindSpeed(double dist)
+    {
+        return (dist / time) * 60;
+    }
+    public override double FindPace(double speed)
+    {
+        return 60 / speed;
+    }
+    public override string GetSummary()
+    {
+        return $"{date} Swimming ({time}min)-Distance {FindDistance(laps)} miles,Speed {FindSpeed(FindDistance(laps))}mph, Pace {FindPace(FindSpeed(FindDistance(laps)))}min per mile";
+    }
 }
