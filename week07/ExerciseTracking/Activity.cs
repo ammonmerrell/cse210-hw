12ed00a615bfbd1df8c6c6f6a7dbc3ac710e93c7
ammonmerrell <ammonmerrell.outlook.com>
2025-06-16 15:14:27 -0600

    made the classes

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel;
+
+public class Activity
+{
+    private DateTime _date = DateTime.Now;
+    private int _time;
+    private int _distance;
+    private double _speed;
+    private double _pace;
+    public string SetDate()
+    {
+        return _date.ToShortDateString();
+    }
+    public int SetTime(int time)
+    {
+        _time = time;
+        return _time;
+    }
+    public int SetDistance(int dist)
+    {
+        _distance = dist;
+        return _distance;
+    }
+    public virtual double FindDistance(double pace)
+    {
+        return pace * 50 / 1000 * 0.62;
+    }
+    public double SetSpeed(double speed)
+    {
+        _speed = speed;
+        return _speed;
+    }
+    public virtual double FindSpeed(double pace)
+    {
+        return 60 / pace;
+    }
+
+    public double SetPace(double pace)
+    {
+        _pace = pace;
+        return _pace;
+    }
+    public virtual double FindPace(double speed)
+    {
+        return 60 / speed;
+    }
+    public virtual string GetSummary()
+    {
+        return $"{SetDate()} activity ({_time})-Distance {_distance},Speed{_speed}mph,Pace{_pace}min per mile";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Running run = new Running();
+        Cycling cyc = new Cycling();
+        Swimming swi = new Swimming();
+        
     }
 }
